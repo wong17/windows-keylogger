071bb4ed4d61ece2d39faca1e59e61b35cc1b147
Wong <josuewong797@gmail.com>
2025-01-16 22:04:39 -0600

    Se mueve lógica de MainWindow a MainWindowController

---
+++ Keylogger/Keylogger/Controllers/MainWindowController.cs
@@ -0,0 +1,155 @@
+﻿using Keylogger.Helpers.Enums;
+using Keylogger.Hooks;
+using Keylogger.Services;
+using Keylogger.Views;
+using Keylogger.WMI;
+using static Keylogger.Helpers.InputMessageHelper;
+using static Keylogger.Helpers.KeyFilterHelper;
+using static Keylogger.Helpers.MouseButtonFilterHelper;
+
+namespace Keylogger.Controllers
+{
+    internal class MainWindowController
+    {
+        private readonly MainWindow _mainWindow;
+
+        private readonly HookService _hookService = new();
+
+        private KeyFilter _selectedKeyFilter = KeyFilter.PrintableCharacters;
+        private MouseButtonFilter _selectedMouseButtonFilter = MouseButtonFilter.All;
+
+        public MainWindowController(MainWindow mainWindow)
+        {
+            _mainWindow = mainWindow;
+
+            SetupEvents();
+
+            _mainWindow.ToolStripStatusLbl.Text = $"{Environment.MachineName} | {Win32OperatingSystem.GetOSVersion()}";
+
+            SetupKeyboardHookControls();
+            SetupMouseHookControls();
+
+            KeyboardHook.LogKeyCallback = LogKeyboardHook;
+            MouseHook.LogMouseButtonsCallback = LogMouseButtons;
+            MouseHook.LogMousePositionCallback = LogMousePosition;
+        }
+
+        private void SetupEvents()
+        {
+            _mainWindow.Load += (_, _) => _hookService.InstallHooks();
+            _mainWindow.FormClosing += (_, _) => _hookService.UninstallHooks();
+
+            _mainWindow.CmbBoxKeyboardEvents.SelectedIndexChanged += (_, _) => UpdateSelectedKeyboardEventPredicate();
+            _mainWindow.CmbBoxKeyFilter.SelectedIndexChanged += (_, _) => UpdateSelectedKeyFilter();
+            _mainWindow.CmbBoxCaptureMouseButtons.SelectedIndexChanged += (_, _) => UpdateSelectedMouseButtonFilter();
+            _mainWindow.CmbBoxMouseEvents.SelectedIndexChanged += (_, _) => UpdateSelectedMouseEventPredicate();
+        }
+
+        private void SetupKeyboardHookControls()
+        {
+            _mainWindow.CmbBoxKeyboardEvents.Items.AddRange([.. KeyboardEvents.Keys.Take(2)]);
+            _mainWindow.CmbBoxKeyboardEvents.SelectedIndex = 0; // Default to "Key Down"
+            UpdateSelectedKeyboardEventPredicate();
+
+            _mainWindow.CmbBoxKeyFilter.Items.AddRange([.. KeyFilters.Keys]);
+            _mainWindow.CmbBoxKeyFilter.SelectedIndex = 1; // Default to "Printable characters"
+        }
+
+        private void SetupMouseHookControls()
+        {
+            _mainWindow.CmbBoxMouseEvents.Items.AddRange([.. MouseEvents]);
+            _mainWindow.CmbBoxMouseEvents.SelectedIndex = 0; // Default to "Mouse Up & Down"
+            UpdateSelectedMouseEventPredicate();
+
+            _mainWindow.CmbBoxCaptureMouseButtons.Items.AddRange([.. MouseButtonFilters.Keys]);
+            _mainWindow.CmbBoxCaptureMouseButtons.SelectedIndex = 0; // Default to "All"
+        }
+
+        private void LogKeyboardHook(string message, string key, KeyType keyType)
+        {
+            if (ShouldDisplayKey(key, keyType, _selectedKeyFilter))
+            {
+                _mainWindow.RichTxtKeyboard.AppendText($"{key}");
+            }
+        }
+
+        private void LogMouseButtons(string mouseButton, string position, string processName)
+        {
+            if (ShouldDisplayMouseButton(mouseButton, _selectedMouseButtonFilter))
+            {
+                var time = DateTime.Now.ToString("hh:mm:ss tt");
+                var format = "{0,-15}{1,-21}{2,18}{3,30}";
+                var finalMessage = string.Format(format, time, mouseButton, position, processName);
+
+                _mainWindow.RichTxtMouseButtons.AppendText(finalMessage + Environment.NewLine);
+            }
+        }
+
+        private void LogMousePosition(string message, string position, string processName)
+        {
+            string time = DateTime.Now.ToString("hh:mm:ss tt");
+            var format = "{0,-15}{1,-13}{2,18}{3,30}";
+            var finalMessage = string.Format(format, time, message, position, processName);
+
+            _mainWindow.RichTxtMousePosition.AppendText(finalMessage + Environment.NewLine);
+        }
+
+        private void UpdateSelectedKeyboardEventPredicate()
+        {
+            var selectedKey = _mainWindow.CmbBoxKeyboardEvents.SelectedItem?.ToString();
+            if (selectedKey is not null && KeyboardEvents.TryGetValue(selectedKey, out var predicate))
+            {
+                KeyboardHook.SelectedKeyboardEventPredicate = predicate;
+            }
+        }
+
+        private void UpdateSelectedKeyFilter()
+        {
+            var selectedKey = _mainWindow.CmbBoxKeyFilter.SelectedItem?.ToString();
+            if (selectedKey is not null && KeyFilters.TryGetValue(selectedKey, out var filter))
+            {
+                _selectedKeyFilter = filter;
+                _mainWindow.CmbBoxKeyboardEvents.Items.Clear();
+
+                if (filter == KeyFilter.FunctionKeys || filter == KeyFilter.ModifierKeys)
+                {
+                    _mainWindow.CmbBoxKeyboardEvents.Items.AddRange([.. KeyboardEvents.Keys.TakeLast(2)]);
+                    _mainWindow.CmbBoxKeyboardEvents.SelectedIndex = 0; // Default to "Key Down & Sys Key Down"
+                    UpdateSelectedKeyboardEventPredicate();
+                    return;
+                }
+
+                if (filter == KeyFilter.PrintableCharacters || filter == KeyFilter.NonPrintableKeys)
+                {
+                    _mainWindow.CmbBoxKeyboardEvents.Items.AddRange([.. KeyboardEvents.Keys.Take(2)]);
+                    _mainWindow.CmbBoxKeyboardEvents.SelectedIndex = 0; // Default to "Key Down"
+                    UpdateSelectedKeyboardEventPredicate();
+                    return;
+                }
+
+                _mainWindow.CmbBoxKeyboardEvents.Items.AddRange([.. KeyboardEvents.Keys]);
+                _mainWindow.CmbBoxKeyboardEvents.SelectedIndex = 0; // Default to "Key Down"
+                UpdateSelectedKeyboardEventPredicate();
+            }
+        }
+
+        private void UpdateSelectedMouseButtonFilter()
+        {
+            var selectedKey = _mainWindow.CmbBoxCaptureMouseButtons.SelectedItem?.ToString();
+            if (selectedKey is not null && MouseButtonFilters.TryGetValue(selectedKey, out var filter))
+            {
+                _selectedMouseButtonFilter = filter;
+            }
+        }
+
+        private void UpdateSelectedMouseEventPredicate()
+        {
+            var selectedKey = _mainWindow.CmbBoxMouseEvents.SelectedItem?.ToString();
+            if (selectedKey is not null &&
+                MouseEventPredicates.TryGetValue((_selectedMouseButtonFilter, selectedKey), out var predicate))
+            {
+                MouseHook.SelectedMouseEventPredicate = predicate;
+            }
+        }
+    }
+}
--- Keylogger/Keylogger/Views/MainWindow.Designer.cs
+++ Keylogger/Keylogger/Views/MainWindow.Designer.cs
@@ -29,15 +29,12 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            RichTxtKeyboard = new RichTextBox();
             RichTxtMouseButtons = new RichTextBox();
             label2 = new Label();
             label3 = new Label();
             CmbBoxKeyboardEvents = new ComboBox();
             label4 = new Label();
             label5 = new Label();
-            RichTxtMousePosition = new RichTextBox();
-            LblOS = new Label();
             label6 = new Label();
             CmbBoxKeyFilter = new ComboBox();
             statusStrip1 = new StatusStrip();
@@ -46,32 +43,24 @@
             CmbBoxCaptureMouseButtons = new ComboBox();
             label8 = new Label();
             CmbBoxMouseEvents = new ComboBox();
+            RichTxtKeyboard = new RichTextBox();
+            RichTxtMousePosition = new RichTextBox();
+            panel1 = new Panel();
+            panel2 = new Panel();
             statusStrip1.SuspendLayout();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(24, 17);
+            label1.Location = new Point(17, 9);
             label1.Name = "label1";
             label1.Size = new Size(76, 21);
             label1.TabIndex = 0;
             label1.Text = "Keyboard";
-            // 
-            // RichTxtKeyboard
-            // 
-            RichTxtKeyboard.BackColor = SystemColors.ControlLight;
-            RichTxtKeyboard.BorderStyle = BorderStyle.None;
-            RichTxtKeyboard.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            RichTxtKeyboard.HideSelection = false;
-            RichTxtKeyboard.Location = new Point(24, 97);
-            RichTxtKeyboard.Name = "RichTxtKeyboard";
-            RichTxtKeyboard.ReadOnly = true;
-            RichTxtKeyboard.Size = new Size(562, 541);
-            RichTxtKeyboard.TabIndex = 1;
-            RichTxtKeyboard.TabStop = false;
-            RichTxtKeyboard.Text = "";
             // 
             // RichTxtMouseButtons
             // 
@@ -79,10 +68,10 @@
             RichTxtMouseButtons.BorderStyle = BorderStyle.None;
             RichTxtMouseButtons.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             RichTxtMouseButtons.HideSelection = false;
-            RichTxtMouseButtons.Location = new Point(609, 97);
+            RichTxtMouseButtons.Location = new Point(17, 78);
             RichTxtMouseButtons.Name = "RichTxtMouseButtons";
             RichTxtMouseButtons.ReadOnly = true;
-            RichTxtMouseButtons.Size = new Size(562, 241);
+            RichTxtMouseButtons.Size = new Size(615, 322);
             RichTxtMouseButtons.TabIndex = 3;
             RichTxtMouseButtons.TabStop = false;
             RichTxtMouseButtons.Text = "";
@@ -91,7 +80,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(609, 17);
+            label2.Location = new Point(17, 21);
             label2.Name = "label2";
             label2.Size = new Size(57, 21);
             label2.TabIndex = 2;
@@ -100,7 +89,7 @@
             // label3
             // 
             label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label3.Location = new Point(24, 58);
+            label3.Location = new Point(754, 10);
             label3.Name = "label3";
             label3.Size = new Size(106, 22);
             label3.TabIndex = 4;
@@ -110,16 +99,15 @@
             // 
             CmbBoxKeyboardEvents.DropDownStyle = ComboBoxStyle.DropDownList;
             CmbBoxKeyboardEvents.FormattingEnabled = true;
-            CmbBoxKeyboardEvents.Location = new Point(136, 57);
+            CmbBoxKeyboardEvents.Location = new Point(866, 9);
             CmbBoxKeyboardEvents.Name = "CmbBoxKeyboardEvents";
             CmbBoxKeyboardEvents.Size = new Size(175, 23);
             CmbBoxKeyboardEvents.TabIndex = 1;
-            CmbBoxKeyboardEvents.SelectedIndexChanged += CmbBoxKeyboardEvents_SelectedIndexChanged;
             // 
             // label4
             // 
             label4.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label4.Location = new Point(609, 57);
+            label4.Location = new Point(17, 53);
             label4.Name = "label4";
             label4.Size = new Size(113, 22);
             label4.TabIndex = 6;
@@ -128,41 +116,18 @@
             // label5
             // 
             label5.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label5.Location = new Point(609, 355);
+            label5.Location = new Point(670, 53);
             label5.Name = "label5";
             label5.Size = new Size(162, 22);
             label5.TabIndex = 7;
             label5.Text = "Cursor position:";
             // 
-            // RichTxtMousePosition
-            // 
-            RichTxtMousePosition.BackColor = SystemColors.ControlLight;
-            RichTxtMousePosition.BorderStyle = BorderStyle.None;
-            RichTxtMousePosition.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            RichTxtMousePosition.HideSelection = false;
-            RichTxtMousePosition.Location = new Point(609, 397);
-            RichTxtMousePosition.Name = "RichTxtMousePosition";
-            RichTxtMousePosition.ReadOnly = true;
-            RichTxtMousePosition.Size = new Size(562, 241);
-            RichTxtMousePosition.TabIndex = 8;
-            RichTxtMousePosition.TabStop = false;
-            RichTxtMousePosition.Text = "";
-            // 
-            // LblOS
-            // 
-            LblOS.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            LblOS.Location = new Point(24, 657);
-            LblOS.Name = "LblOS";
-            LblOS.Size = new Size(562, 22);
-            LblOS.TabIndex = 11;
-            LblOS.TextAlign = ContentAlignment.TopRight;
-            // 
             // label6
             // 
             label6.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label6.Location = new Point(362, 58);
+            label6.Location = new Point(1056, 10);
             label6.Name = "label6";
-            label6.Size = new Size(43, 22);
+            label6.Size = new Size(47, 22);
             label6.TabIndex = 12;
             label6.Text = "Show:";
             // 
@@ -170,18 +135,17 @@
             // 
             CmbBoxKeyFilter.DropDownStyle = ComboBoxStyle.DropDownList;
             CmbBoxKeyFilter.FormattingEnabled = true;
-            CmbBoxKeyFilter.Location = new Point(411, 57);
+            CmbBoxKeyFilter.Location = new Point(1109, 11);
             CmbBoxKeyFilter.Name = "CmbBoxKeyFilter";
             CmbBoxKeyFilter.Size = new Size(175, 23);
             CmbBoxKeyFilter.TabIndex = 2;
-            CmbBoxKeyFilter.SelectedIndexChanged += CmbBoxKeyFilter_SelectedIndexChanged;
             // 
             // statusStrip1
             // 
             statusStrip1.Items.AddRange(new ToolStripItem[] { ToolStripStatusLbl });
-            statusStrip1.Location = new Point(0, 648);
+            statusStrip1.Location = new Point(0, 821);
             statusStrip1.Name = "statusStrip1";
-            statusStrip1.Size = new Size(1204, 22);
+            statusStrip1.Size = new Size(1296, 22);
             statusStrip1.TabIndex = 13;
             statusStrip1.Text = "statusStrip1";
             // 
@@ -193,9 +157,9 @@
             // label7
             // 
             label7.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label7.Location = new Point(952, 59);
+            label7.Location = new Point(1071, 23);
             label7.Name = "label7";
-            label7.Size = new Size(57, 22);
+            label7.Size = new Size(51, 23);
             label7.TabIndex = 16;
             label7.Text = "Buttons:";
             // 
@@ -203,18 +167,17 @@
             // 
             CmbBoxCaptureMouseButtons.DropDownStyle = ComboBoxStyle.DropDownList;
             CmbBoxCaptureMouseButtons.FormattingEnabled = true;
-            CmbBoxCaptureMouseButtons.Location = new Point(1015, 58);
+            CmbBoxCaptureMouseButtons.Location = new Point(1128, 23);
             CmbBoxCaptureMouseButtons.Name = "CmbBoxCaptureMouseButtons";
             CmbBoxCaptureMouseButtons.Size = new Size(156, 23);
             CmbBoxCaptureMouseButtons.TabIndex = 17;
-            CmbBoxCaptureMouseButtons.SelectedIndexChanged += CmbBoxCaptureMouseButtons_SelectedIndexChanged;
             // 
             // label8
             // 
             label8.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label8.Location = new Point(750, 57);
+            label8.Location = new Point(857, 21);
             label8.Name = "label8";
-            label8.Size = new Size(45, 20);
+            label8.Size = new Size(44, 20);
             label8.TabIndex = 18;
             label8.Text = "Event:";
             // 
@@ -222,41 +185,86 @@
             // 
             CmbBoxMouseEvents.DropDownStyle = ComboBoxStyle.DropDownList;
             CmbBoxMouseEvents.FormattingEnabled = true;
-            CmbBoxMouseEvents.Location = new Point(801, 58);
+            CmbBoxMouseEvents.Location = new Point(907, 21);
             CmbBoxMouseEvents.Name = "CmbBoxMouseEvents";
             CmbBoxMouseEvents.Size = new Size(134, 23);
             CmbBoxMouseEvents.TabIndex = 19;
-            CmbBoxMouseEvents.SelectedIndexChanged += CmbBoxMouseEvents_SelectedIndexChanged;
+            // 
+            // RichTxtKeyboard
+            // 
+            RichTxtKeyboard.BackColor = SystemColors.ControlLight;
+            RichTxtKeyboard.BorderStyle = BorderStyle.None;
+            RichTxtKeyboard.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            RichTxtKeyboard.HideSelection = false;
+            RichTxtKeyboard.Location = new Point(17, 40);
+            RichTxtKeyboard.Name = "RichTxtKeyboard";
+            RichTxtKeyboard.ReadOnly = true;
+            RichTxtKeyboard.Size = new Size(1267, 348);
+            RichTxtKeyboard.TabIndex = 13;
+            RichTxtKeyboard.TabStop = false;
+            RichTxtKeyboard.Text = "";
+            // 
+            // RichTxtMousePosition
+            // 
+            RichTxtMousePosition.BackColor = SystemColors.ControlLight;
+            RichTxtMousePosition.BorderStyle = BorderStyle.None;
+            RichTxtMousePosition.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            RichTxtMousePosition.HideSelection = false;
+            RichTxtMousePosition.Location = new Point(670, 78);
+            RichTxtMousePosition.Name = "RichTxtMousePosition";
+            RichTxtMousePosition.ReadOnly = true;
+            RichTxtMousePosition.Size = new Size(614, 322);
+            RichTxtMousePosition.TabIndex = 20;
+            RichTxtMousePosition.TabStop = false;
+            RichTxtMousePosition.Text = "";
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.Controls.Add(RichTxtKeyboard);
+            panel1.Controls.Add(CmbBoxKeyboardEvents);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(CmbBoxKeyFilter);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1296, 404);
+            panel1.TabIndex = 21;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(RichTxtMouseButtons);
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(CmbBoxMouseEvents);
+            panel2.Controls.Add(RichTxtMousePosition);
+            panel2.Controls.Add(CmbBoxCaptureMouseButtons);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label8);
+            panel2.Location = new Point(0, 404);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1296, 414);
+            panel2.TabIndex = 22;
             // 
             // MainWindow
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1204, 670);
-            Controls.Add(CmbBoxMouseEvents);
-            Controls.Add(label8);
-            Controls.Add(CmbBoxCaptureMouseButtons);
-            Controls.Add(label7);
+            ClientSize = new Size(1296, 843);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Controls.Add(statusStrip1);
-            Controls.Add(CmbBoxKeyFilter);
-            Controls.Add(label6);
-            Controls.Add(LblOS);
-            Controls.Add(RichTxtMousePosition);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(CmbBoxKeyboardEvents);
-            Controls.Add(label3);
-            Controls.Add(RichTxtMouseButtons);
-            Controls.Add(label2);
-            Controls.Add(RichTxtKeyboard);
-            Controls.Add(label1);
             Name = "MainWindow";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Windows Keylogger - D. Wong";
-            FormClosing += MainWindow_FormClosing;
-            Load += MainWindow_Load;
             statusStrip1.ResumeLayout(false);
             statusStrip1.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -266,20 +274,21 @@
         private Label label1;
         private Label label2;
         private Label label3;
-        private ComboBox CmbBoxKeyboardEvents;
-        private RichTextBox RichTxtKeyboard;
-        private RichTextBox RichTxtMouseButtons;
         private Label label4;
         private Label label5;
-        private RichTextBox RichTxtMousePosition;
-        private Label LblOS;
         private Label label6;
-        private ComboBox CmbBoxKeyFilter;
-        private StatusStrip statusStrip1;
-        private ToolStripStatusLabel ToolStripStatusLbl;
+        internal ToolStripStatusLabel ToolStripStatusLbl;
         private Label label7;
-        private ComboBox CmbBoxCaptureMouseButtons;
         private Label label8;
-        private ComboBox CmbBoxMouseEvents;
+        internal ComboBox CmbBoxKeyboardEvents;
+        internal ComboBox CmbBoxKeyFilter;
+        internal ComboBox CmbBoxMouseEvents;
+        internal RichTextBox RichTxtMouseButtons;
+        internal ComboBox CmbBoxCaptureMouseButtons;
+        internal StatusStrip statusStrip1;
+        internal RichTextBox RichTxtKeyboard;
+        internal RichTextBox RichTxtMousePosition;
+        private Panel panel1;
+        private Panel panel2;
     }
 }
